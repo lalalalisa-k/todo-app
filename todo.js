0362b47add4424d8b7d62db3cf71e9e642858c36
lalalalisa-k <programmerasari@gmail.com>
2021-04-29 16:25:45 +0900

    2021/04/29

--- todo.js
+++ todo.js
@@ -1,139 +1,103 @@
 'use strict';
 {
   const todoItems = [];
-  const todoItem = document.getElementById("input-todo-box");
+  const todoItem = document.getElementById('input-todo-box');
   const addButton = document.getElementById('add-button');
-  const table = document.createElement('table');
-  const tr = document.createElement('tr');
-  const th = document.createElement('th');
-  const td = document.createElement('td');
-  let todo = todoItem.value;
+  const table = document.getElementById('table');
 
-  const todos = {
-    //id: todoItems.length, task: todoItem.value, status: '作業中'
-  }
+  
 
-
-  const addTask = () => {
-    addButton.addEventListener('click', () => {
-      let todo = todoItem.value;
-      if (todo)
-      {
-        todoItems.push(todos);
-        todos.id = todoItems.length;
-        todos.task = todoItem.value;
-        todos.status = '作業中';
-
-        document.getElementById('wrapper').appendChild(table);
-        const reset = document.getElementById("input-todo-box");
-        reset.value = '';
-        showTask();
-      } else
-      {
-        alert("入力して下さい");
-      }
-    });
-  }
-
-  const createProgressButton = () => {
+  //yoshida修正 createProgressButton
+  const createProgressButton = (todo, status, row) => {
     const progressButton = document.createElement('button');
-    progressButton.innerText = '作業中';
-    table.appendChild(tr);
-    //tr.appendChild(th);
-    //th.appendChild(td);
-    td.appendChild(progressButton);
-
+    progressButton.innerText = todo.status;
+    status.appendChild(progressButton);
     progressButton.addEventListener('click', () => {
-      if (progressButton.innerText === '作業中')
-      {
-        progressButton.innerText = '完了';
-        todos.status = "done"; //プロパティ追加
-      } else
-      {
-        progressButton.innerText = '作業中';
-        todos.status = "progress" //プロパティ追加
+      if (progressButton.innerText === '作業中') {
+        todo.status = '完了';
+        progressButton.innerText = todo.status;
+        row.classList.add('finish');
+        return;
       }
+      todo.status = '作業中';
+      progressButton.innerText = todo.status;
+      row.classList.remove('finish');
     });
   }
 
-  const createRemoveButton = (id) => {
-
+  const createRemoveButton = (remove, row) => {
     const removeButton = document.createElement('button');
     removeButton.innerText = '削除';
-    table.appendChild(tr);
-    tr.appendChild(th);
-    th.appendChild(td);
-    td.appendChild(removeButton);
-
+    remove.appendChild(removeButton);
     removeButton.addEventListener('click', () => {
-
-      td.removeChild(removeButton);
-      tr.removeChild(td);
-      //td.removeChild(table);
-
-      const todoId = todoItems.findIndex(todos => {
-        return todos.id === id;
-      });
-      todoItems.splice(todoId, 1);
-      todoItems.forEach((value, index) => {
-        todoItems[index].id = index;
-      });
+      //削除ボタンを押したとき、押した要素を配列todosから削除する。showTasksで現在のtodosの状態で一覧を作り直す。
+      const index = row.rowIndex - 1;
+      todoItems.splice(index, 1);
+      showTasks();
     });
   }
 
-  const showTask = () => {
+  //yoshida showTask修正
+  const showTasks = () => {
+    //forEachを回すので、前回残っている表はループ前に削除する
+    table.innerText = '';
+    todoItems.forEach(todo => {
+      //ループにこのように記述することで、forEachが走るたびにlengthが連番になります。
+      const todoId = table.rows.length;
+      //最後の行に新しい行を追加
+      const row = table.insertRow(-1);
+      row.classList.add('tasks');
+      if (todo.status === '完了') {
+        row.classList.add('finish');
+      }
 
-    const tr = document.createElement('tr');
-    const th = document.createElement('th');
-    //const td = document.createElement('td');
+      //各項目の追加
+      //id content status removeの順番でtdが来るように指定
+      const id = row.insertCell(0);
+      const content = row.insertCell(1);
+      const status = row.insertCell(2);
+      const remove = row.insertCell(3);
 
-    //tr.appendChild(th);
-    //tr.appendChild(td);
-    //th.appendChild(td);
-
-    for (let i = 0; i < todoItems.length ; i++)
-    {
-      table.appendChild(tr);
-      tr.appendChild(th);
-      //th.appendChild(td);
-      th.textContent = todos['id'] + " " + todos['task'] + " " + todos['status'];
-
-    }
-
-    //tr.appendChild(td);
-    //tr.appendChild(th);
-    createProgressButton();
-    createRemoveButton();
+      id.innerText = todoId;
+      content.innerText = todo.task;
+      //createProgressButtonにtodo→ボタン切り替えに必要。→適切な位置に配置するために使用 row→classをつけるのに使用
+      createProgressButton(todo, status, row);
+      createRemoveButton(remove, row);
+    });
   }
-
-  const radioButtonAll = document.getElementById('radio-all');
-  const radioButtonWorking = document.getElementById('radio-working');
-  const radioButtonDone = document.getElementById('radio-done');
 
   const radioFilter = () => {
-    let flag = false;
+    //table.innerText = '';
     const radioForm = document.getElementById("radio-form");
-    for (let i = 0; i < radioForm.length; i++)
+    const radioButtonAll = document.getElementById("radio-all");
+    const radioButtonWorking = document.getElementById("radio-working");
+    const radioButtonDone = document.getElementById("radio-done");
+
+    if (radioButtonAll.checked){
+      return showTasks();
+    }
+    else if (radioButtonWorking.checked){
+        const doingTodos = todoItems.filter((todo => todo.status) === '作業中')
+      showTasks(doingTodos);
+      console.log("checked");
+    }
+    else if (radioButtonDone.checked){
+      const doneTodos = todoItems.filter((todo => todo.status) === '完了')
+      showTasks(doneTodos);
+      console.log("checked");
+    } else
     {
-      if (radioButtonAll.checked)
-      {
-        flag = true;
-        console.log("all");
-
-        return showTask();
-
-      } else if (radioButtonDone.checked)
-      {
-
-      } else if (radioButtonDone.checked)
-      {
-
-      }
+      showTasks();
     }
 
   }
 
-  radioFilter();
-  addTask();
-
+  //yoshida追加
+  addButton.addEventListener('click', () => {
+    const todo = { task: todoItem.value, status: '作業中' };
+    todoItems.push(todo);
+    //showTasks();
+    radioFilter();
+    todoItem.value = '';
+  });
 }
